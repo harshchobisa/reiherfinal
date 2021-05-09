e5c530187108eedd4695aa6bb2b4c22d3e92713c
Harsh Chobisa <harsh.chobisa@gmail.com>
2021-05-09 01:12:20 -0700

    changed all username fields to email

--- app/models.py
+++ app/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 # Create your models here.
 class Users(models.Model):
-    username = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
     password = models.CharField(max_length=200)
     role = models.CharField(max_length=10)
 
 class UserAuthTokens(models.Model):
-    username = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
     token = models.CharField(max_length=200)
     timestamp = models.CharField(max_length=50)
--- app/views.py
+++ app/views.py
@@ -19,20 +19,20 @@ def create_user(request):
     #input validation
     try:
         payload = json.loads(request.body)
-        username = payload["username"]
+        email = payload["email"]
         password = payload["password"]
         role = payload["role"]
     except:
-        return HttpResponse("missing/blank username, password, or role", status=401)
+        return HttpResponse("missing/blank email, password, or role", status=401)
 
-    if (username == "" or password == "" or role == ""):
-        return HttpResponse("missing/blank username, password, or role", status=401)
+    if (email == "" or password == "" or role == ""):
+        return HttpResponse("missing/blank email, password, or role", status=401)
 
-    #make sure username isn't already taken
+    #make sure email isn't already taken
     try:
-        user_count = Users.objects.filter(username=username).count() 
+        user_count = Users.objects.filter(email=email).count() 
         if user_count != 0:
-            return HttpResponse("username already in use", status=406)
+            return HttpResponse("email already in use", status=406)
     except Exception as e:
         return HttpResponse(status=401)
 
@@ -48,7 +48,7 @@ def create_user(request):
 
 
     #save user in database
-    user = Users(username=username, password=hashedPassword, role=role)
+    user = Users(email=email, password=hashedPassword, role=role)
     try:
         user.save()
         return HttpResponse("user succesfully created", status=201)
@@ -66,10 +66,10 @@ def login(request):
     #input validation
     try:
         payload = json.loads(request.body)
-        username = payload['username']
+        email = payload['email']
         password = payload['password']
     except:
-        return HttpResponse("missing/blank username or password", status=401)
+        return HttpResponse("missing/blank email or password", status=401)
 
     #hash password
     key = 'b\'\\xd3\\xf4\\xb7X\\xbd\\x07"\\xf4a\\\'\\xf5\\x16\\xd7a\\xa4\\xbd\\xf0\\xe7\\x10\\xdeR\\x0el\\xc2fW\\x80\\xfd\\xd39\\x953\''
@@ -82,16 +82,16 @@ def login(request):
 
     #make sure user exists
     try:
-        user_count = Users.objects.filter(username=username, password=hashedPassword).count()
+        user_count = Users.objects.filter(email=email, password=hashedPassword).count()
         if user_count != 1:
             return HttpResponse("unable to find user", status=404)
     except:
         return HttpResponse("unable to find user", status=404)
 
     #create authentication token for session
-    token = createAuthToken(username)
+    token = createAuthToken(email)
 
-    request.session['username'] = username
+    request.session['email'] = email
     request.session['token'] = str(token)
     request.session.set_expiry(3600) #session expires in 3600 seconds = 1 hour
 
@@ -102,7 +102,7 @@ def login(request):
 @csrf_exempt
 def auth_test(request):
     try:
-        if checkAuthToken(request.session['username'], request.session['token']):
+        if checkAuthToken(request.session['email'], request.session['token']):
             return HttpResponse("authenticated", status=200)
     except:
         return HttpResponse("not authenticated", status=404)
@@ -110,10 +110,10 @@ def auth_test(request):
     return HttpResponse("not authenticated", status=404)
 
 #checks that the user's session is authenticated
-def checkAuthToken(username, token):
+def checkAuthToken(email, token):
     try:
         #get the most recent token entry in the database for the user
-        userAndToken = UserAuthTokens.objects.filter(username=username).order_by('-timestamp')[0]
+        userAndToken = UserAuthTokens.objects.filter(email=email).order_by('-timestamp')[0]
         
         tokenbytes = bytes(userAndToken.token, 'utf-8')
 
@@ -131,10 +131,10 @@ def checkAuthToken(username, token):
         return False
 
 #creates the authentication token for sessions
-def createAuthToken(username):
+def createAuthToken(email):
     #create secure 32 byte token
     token = str(token_bytes(32))
-    userAndToken = UserAuthTokens(username=username, token=token, timestamp=time.time())
+    userAndToken = UserAuthTokens(email=email, token=token, timestamp=time.time())
     
     #save token in database for comparison
     userAndToken.save()
