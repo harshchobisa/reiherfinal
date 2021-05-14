e8108aace1f05953032e298b126ffc0dff3c7c5e
Prithvi Kannan <46332835+prithvikannan@users.noreply.github.com>
2021-05-14 10:37:20 -0700

    redirect from LP if logged in

--- frontend/src/Pages/LandingPage.js
+++ frontend/src/Pages/LandingPage.js
@@ -1,13 +1,46 @@
 import React, { Component } from "react";
 import { Container, Button } from "react-bootstrap";
+import { Redirect } from "react-router-dom";
+import axios from "axios";
+
 export default class LandingPage extends Component {
-    render() {
-        return (
-            <Container>
-                <h1>Welcome to MentorSEAS!</h1>
-                <Button href="#/login" variant="outline-primary">Sign In </Button>{' '}
-                <Button href="#/signup" variant="outline-secondary">Sign Up</Button>{' '}
-            </Container>
-        );
+  state = {
+    loggedIn: false,
+  };
+
+  componentDidMount() {
+    axios({
+      method: "post",
+      url: "//localhost:8000/getCurrentUser/",
+      headers: {
+        "Content-Type": "text/plain",
+      },
+      withCredentials: true,
+    })
+      .then((response) => {
+        if (response.data !== "") {
+          this.setState({ loggedIn: true });
+        }
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
+  render() {
+    if (this.state.loggedIn) {
+      return <Redirect to="/home" />;
     }
+    return (
+      <Container>
+        <h1>Welcome to MentorSEAS!</h1>
+        <Button href="#/login" variant="outline-primary">
+          Sign In{" "}
+        </Button>{" "}
+        <Button href="#/signup" variant="outline-secondary">
+          Sign Up
+        </Button>{" "}
+      </Container>
+    );
+  }
 }
