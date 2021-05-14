439c20bc7810a2b13b7178b2146850f1e1f0c585
Prithvi Kannan <46332835+prithvikannan@users.noreply.github.com>
2021-05-14 13:56:12 -0700

    added dummy user populate to admin page

--- frontend/src/Pages/AdminPage.js
+++ frontend/src/Pages/AdminPage.js
@@ -6,6 +6,7 @@ import { JsonToTable } from "react-json-to-table";
 export default class AdminPage extends Component {
   state = {
     families: [],
+    dummy: 0,
   };
   componentDidMount() {
     var config = {
@@ -27,7 +28,7 @@ export default class AdminPage extends Component {
       });
   }
 
-  onSubmit = () => {
+  runPairings = () => {
     var config = {
       method: "post",
       url: "//localhost:8000/createFamilies/",
@@ -45,22 +46,53 @@ export default class AdminPage extends Component {
       .catch(function (error) {
         console.log(error);
       });
+
+    window.location.reload();
+  };
+
+  addData = () => {
+    var config = {
+      method: "post",
+      url: "//localhost:8000/populateUsers/",
+      headers: {
+        "Content-Type": "text/plain",
+      },
+      data: JSON.stringify({
+        num_mentors: 50,
+        num_mentees: 150,
+      }),
+      withCredentials: true,
+    };
+
+    axios(config)
+      .then(function (response) {
+        console.log(response.status);
+        console.log(JSON.stringify(response.data));
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+
+    window.location.reload();
   };
 
   render() {
     return (
       <Container>
         <h1>This is the admin page</h1>
+        <Button variant="outline-secondary" onClick={this.runPairings}>
+          Run pairings
+        </Button>{" "}
+        <Button variant="outline-secondary" onClick={this.addData}>
+          Create users
+        </Button>{" "}
         <p>Here are the families</p>
         {this.state.families.map((family) => (
           <div>
-            <b/>
+            <b />
             <JsonToTable json={family} />
           </div>
         ))}
-        <Button variant="outline-secondary" onClick={this.onSubmit}>
-          Run pairings
-        </Button>{" "}
       </Container>
     );
   }
